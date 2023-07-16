d881403f06bd8be4c0cbc6c104c6d887089bad0e
denisitpro <40316333+denisitpro@users.noreply.github.com>
2023-07-16 10:21:45 +0300

    add support zbx server and client (#51)

---
+++ roles/zabbix-agent/files/scripts/fantom_block_height.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+url_node=$1
+test="$(curl -s -X POST $1 --header "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq .result | sed 's/[^0-9.a-z]//g')"
+echo $(($test))
