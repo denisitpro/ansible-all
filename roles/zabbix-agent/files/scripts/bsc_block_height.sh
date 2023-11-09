43e4b3e855b6e9b4e146e64e497ceb3501e74e44
denisitpro <40316333+denisitpro@users.noreply.github.com>
2023-11-09 12:57:08 +0300

    update role zbx (#64)

--- roles/zabbix-agent/files/scripts/bsc_block_height.sh
+++ roles/zabbix-agent/files/scripts/bsc_block_height.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 url_node=$1
-test2="$(curl -s $1 -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":56}'  | jq .result | sed 's/[^0-9.a-z]//g')"
+test2="$(curl -s $1 -H "x-api-key: zabbix" -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":56}'  | jq .result | sed 's/[^0-9.a-z]//g')"
 echo $(($test2))
--- roles/zabbix-agent/files/scripts/eth_block_height.sh
+++ roles/zabbix-agent/files/scripts/eth_block_height.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 url_node=$1
-test="$(curl -s --header "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":67}' $1 | jq .result | sed 's/[^0-9.a-z]//g')"
+test="$(curl -s -H "x-api-key: zabbix" --header "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":67}' $1 | jq .result | sed 's/[^0-9.a-z]//g')"
 echo $(($test))
